751e014b9c820a13f201c86dae84a7358f51cb27
andrew200473 <andreyke.rodin@yandex.ru>
2022-09-25 00:02:34 +0300

    задачи с первого урока C++ 08.09.22 ConsoleApplication2.cpp consoleApplication3.cpp

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int r1()
+{
+	std::cout << " vvedite x ";
+	int x;
+	std::cin >> x;
+	return x;
+}
+
+int main()
+{
+	int x = r1();
+	std::cout << (log(abs(cos(x)))) / (log(1 + x * 2 * 2)) << std::endl;
+	return 0;
+}
---
+++ consoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿// вторая лаба с первого урока.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double PI = 3.14;
+    std::cout << "введите Х: ";
+    int x;
+    std::cin >> x;
+    int f;
+        if (0 <= x <= 1)
+            f = 2 ^ 2 - x;
+        if (x > 1 || x < 0)
+            f = (pow(x,2) - sin(x) * PI * pow(x,2));
+        std::cout << f << std::endl;
+        return 0;
+}
+
